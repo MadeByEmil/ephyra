a2fe1d2b980d612626242c72107c88d4332cc1ff
Emil Sayahi <limesayahi@gmail.com>
2021-01-14 00:59:26 -0500

    Initial commit 😸

---
+++ src/me.js
@@ -0,0 +1,6 @@
+var me = {
+  "name": "User",
+  "description": "A description.",
+  "picture": "https://i.pinimg.com/564x/de/23/0e/de230e96fe1dc837fe662e838e7b96c7.jpg",
+  "subscriptions": ["bafybeigpmoroovhuadqpjdasnhdnmpvoqkzvu5qvxmrhdd3qrc2jyiooq4"]
+}
