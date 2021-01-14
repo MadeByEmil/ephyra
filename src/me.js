8631367d3cf98bd6c6365485f917daf383badd79
Emil Sayahi <limesayahi@gmail.com>
2021-01-14 01:09:23 -0500

    Use IPFS profile picture

--- src/me.js
+++ src/me.js
@@ -1,6 +1,6 @@
 var me = {
   "name": "User",
   "description": "A description.",
-  "picture": "https://i.pinimg.com/564x/de/23/0e/de230e96fe1dc837fe662e838e7b96c7.jpg",
+  "picture": "ipfs://bafybeiccfclkdtucu6y4yc5cpr6y3yuinr67svmii46v5cfcrkp47ihehy/albums/QXBvbGxvIDE3IE1hZ2F6aW5lIDE0Ny9B/21687952681_e2e01394d2_o.jpg",
   "subscriptions": ["bafybeigpmoroovhuadqpjdasnhdnmpvoqkzvu5qvxmrhdd3qrc2jyiooq4"]
 }
