8d3b2fb100ae58223de2f50fdca505ae9c4d5f53
Emil Sayahi <limesayahi@gmail.com>
2021-01-18 15:11:14 -0500

    Post button is working!

--- src/me.js
+++ src/me.js
@@ -2,5 +2,6 @@ var me = {
   "name": "User",
   "description": "A description.",
   "picture": "ipfs://bafybeiccfclkdtucu6y4yc5cpr6y3yuinr67svmii46v5cfcrkp47ihehy/albums/QXBvbGxvIDE3IE1hZ2F6aW5lIDE0Ny9B/21687952681_e2e01394d2_o.jpg",
-  "subscriptions": ["bafybeigpmoroovhuadqpjdasnhdnmpvoqkzvu5qvxmrhdd3qrc2jyiooq4"]
+  "subscriptions": ["bafybeihuxsxdudtceshrps3odl6mtflg6aeyli3hfrcqygha6reqri2rha"],
+  "posts": []
 }
